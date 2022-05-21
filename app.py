9680e70fa72d58a649f272c477f0bcdd505802d3
rodrigolsc <rodrigo.laugeni@hotmail.com>
2022-05-20 22:51:11 -0300

    Alterando nome desafio devopslab

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)                                                                                                                           
 
 @app.route("/")
 def pagina_inicial():
-    return "Rodrigo Laugeni Scolfaro Caporici"
+    return "Rodrigo Laugeni Scolfaro"
 
 if __name__ == '__main__':
     app.run()
