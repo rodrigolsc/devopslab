1390484d8dc1d2813295a28084ac50f656009bf8
rodrigolsc <rodrigo.laugeni@hotmail.com>
2022-05-20 22:15:16 -0300

    Alterando arquivo app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Rodrigo Laugeni"
+    return "Rodrigo Laugeni Scolfaro"
 
 if __name__ == '__main__':
     app.run()
--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Rodrigo Laugeni")
+        self.assertEqual(self.result.data.decode('utf-8'), "Rodrigo Laugeni Scolfaro")
